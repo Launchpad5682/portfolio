71f1e9cd8359fe5978e03a2075677ce24efe929c
launchpad5682 <launchpad5682@gmail.com>
2021-08-15 23:18:47 +0530

    projects updated

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,31 +1,42 @@
 const projectsList = [
   {
-    name: "fun-with-triangles",
-    repo_url: "https://github.com/Launchpad5682/fun-with-triangles",
-    live_url: "https://fun-with-triangles-launchpad5682.netlify.app/",
+    name: "Do you know me ?",
+    repo_url: "https://github.com/Launchpad5682/Do-you-know-me-CLI-nodejs",
+    live_url: "https://replit.com/@Launchpad5682/Do-you-know-me#index.js",
     description:
-      "This is simple fun project on properties of triangle, including quiz on triangles.",
+      "A simple CLI based quiz app to check how much you know about me?",
   },
   {
-    name: "profit-loss-stocks",
-    repo_url: "https://github.com/Launchpad5682/profit-loss-stocks",
-    live_url: "https://profit-loss-stocks.netlify.app/",
-    description:
-      "A simple application to calculate profit and loss of Stock Prizes.",
+    name: "Marvel Quiz App",
+    repo_url: "https://github.com/Launchpad5682/marvel-quiz-app-nodejs",
+    live_url: "https://replit.com/@Launchpad5682/Marvel-Quiz-App#index.js",
+    description: "A simple CLI based quiz app on Marvel",
   },
   {
-    name: "Birthday Palindrome",
-    repo_url: "https://github.com/Launchpad5682/birthday-palindrome",
-    live_url: "",
+    name: "Minion Speak",
+    repo_url: "https://github.com/Launchpad5682/minion-speak",
+    live_url: "https://minion-speech-translation.netlify.app/",
     description:
-      "A simple application to check whether your birthday is palindrome or not.",
+      "What language do Minions talk? Let's get in and translate the language and understand it.",
   },
   {
-    name: "Birthday Lucky",
-    repo_url: "https://github.com/Launchpad5682/birthday-lucky",
-    live_url: "",
+    name: "Ferb Latin Translator",
+    repo_url: "https://github.com/Launchpad5682/ferb-latin-translator",
+    live_url: "https://ferb-latin-translator-launchpad5682.netlify.app/",
     description:
-      "A simple application to check whether the birth date is lucky or not by using the lucky number to divide it from birth date and if it's divisible then the birth date is lucky.",
+      "What language do Phinese and Ferb talk? Let's get in and translate the language and understand it.",
+  },
+  {
+    name: "Know Your Emotions",
+    repo_url: "https://github.com/Launchpad5682/know-your-emotions",
+    live_url: "https://know-your-emotions.netlify.app/",
+    description: "Enter the emoji and know the emotion of the emoji",
+  },
+  {
+    name: "Good Reads",
+    repo_url: "https://github.com/Launchpad5682/good-reads",
+    live_url: "https://good-books-launchpad5682.netlify.app/",
+    description: "A simple application which only recommends pre-saved books.",
   },
   {
     name: "Cash Register",
@@ -35,23 +46,32 @@ const projectsList = [
       "Cash Register is an application to calculate the change with the denomination of 1, 5, 10, 20, 50, 100, 500,and 2000.",
   },
   {
-    name: "Good Reads",
-    repo_url: "https://github.com/Launchpad5682/good-reads",
-    live_url: "https://good-books-launchpad5682.netlify.app/",
-    description: "A simple application which only recommends pre-saved books.",
-  },
-  {
-    name: "Know Your Emotions",
-    repo_url: "https://github.com/Launchpad5682/know-your-emotions",
-    live_url: "https://know-your-emotions.netlify.app/",
-    description: "Enter the emoji and know the emotion of the emoji",
-  },
-  {
-    name: "Minion Speak",
-    repo_url: "https://github.com/Launchpad5682/minion-speak",
-    live_url: "https://minion-speech-translation.netlify.app/",
+    name: "Birthday Lucky",
+    repo_url: "https://github.com/Launchpad5682/birthday-lucky",
+    live_url: "",
     description:
-      "What language do Minions talk? Let's get in and translate the language and understand it.",
+      "A simple application to check whether the birth date is lucky or not by using the lucky number to divide it from birth date and if it's divisible then the birth date is lucky.",
+  },
+  {
+    name: "fun-with-triangles",
+    repo_url: "https://github.com/Launchpad5682/fun-with-triangles",
+    live_url: "https://fun-with-triangles-launchpad5682.netlify.app/",
+    description:
+      "This is simple fun project on properties of triangle, including quiz on triangles.",
+  },
+  {
+    name: "Birthday Palindrome",
+    repo_url: "https://github.com/Launchpad5682/birthday-palindrome",
+    live_url: "",
+    description:
+      "A simple application to check whether your birthday is palindrome or not.",
+  },
+  {
+    name: "profit-loss-stocks",
+    repo_url: "https://github.com/Launchpad5682/profit-loss-stocks",
+    live_url: "https://profit-loss-stocks.netlify.app/",
+    description:
+      "A simple application to calculate profit and loss of Stock Prizes.",
   },
 ];
 
